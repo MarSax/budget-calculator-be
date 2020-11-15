7d6d37da3f0ca9ffcc3f60d04df2e38f5cce473c
MarSax <msassanelli12@gmail.com>
2020-11-15 11:48:04 +0100

    first commit

---
+++ src/main/java/com/sax/budgetcalculatorbe/BudgetCalculatorBeApplication.java
@@ -0,0 +1,13 @@
+package com.sax.budgetcalculatorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetCalculatorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetCalculatorBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sax/budgetcalculatorbe/BudgetCalculatorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sax.budgetcalculatorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetCalculatorBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
